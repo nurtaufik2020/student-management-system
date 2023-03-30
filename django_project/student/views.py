6f2f4b5093f13d045e4ecb193fd758ce601c37a4
nurtaufik2020 <nurtaufik2020@gmail.com>
2023-03-31 03:56:36 +0700

    31-03-2022

---
+++ django_project/student/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render,redirect,HttpResponseRedirect
+from django.urls import reverse
+from .models import Student
+from .forms import StudentForm
+# Create your views here.
+def index(request):
+    data=Student.objects.all()
+    context={
+        'datas': data
+    }
+    return render(request,'student/index.html',context)
+
+#membuat method view siswa
+def view_student(request,id):
+    student=Student.object.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
+
+def add(request):
+    if request.method == 'POST':
+        form=StudentForm(request.POST)
+        if form.is_valid():
+            new_student_number=form.cleaned_data['student_number']
+            new_first_name=form.cleaned_data['first_name']
+            new_last_name=form.cleaned_data['last_name']
+            new_email=form.cleaned_data['email']
+            new_field_of_study=form.cleaned_data['field_of_study']
+            new_gpa=form.cleaned_data['gpa']
+            
+            new_student=Student(
+                student_number=new_student_number,
+                first_name=new_first_name,
+                last_name=new_last_name,
+                email=new_email,
+                field_of_study=new_field_of_study,
+                gpa=new_gpa
+            )
+            new_student.save()
+            context={
+                'form':StudentForm(),
+                'success':True
+            }
+            return render(request,'student/add.html',context)
+    else:
+        form= StudentForm()
+        return render(request,'student/add.html',{'form':StudentForm()})
+    
+def edit(request,id):
+    if request.method=='POST':
+        student=Student.objects.get(pk=id)
+        form=StudentForm(request.POST,instance=student)
+        if form.is_valid():
+            form.save()
+            context={
+                'form':form,
+                'id':id,
+                'success':True
+            }
+            return render(request,'student/edit.html',context)
+    else:
+        student=Student.objects.get(pk=id)
+        form= StudentForm(instance=student)
+        context={
+                'form':form,
+            }
+    return render(request,'student/edit.html',context)
+
+
+def delete(request,id):
+    if request.method=='POST':
+        student=Student.objects.get(pk=id)
+        student.delete()
+    return HttpResponseRedirect(reverse('index'))
+        
